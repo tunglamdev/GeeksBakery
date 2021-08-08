8597a6d8f750c0dcfc5d3902ca8e1162562f6cde
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2021-08-08 22:31:32 +0700

    Update: Fix code for view Categories

--- App/Views/cakes/categories.php
+++ App/Views/cakes/categories.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Geek's Cakes</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet" />
-    <link rel="stylesheet" href="<?= URL_CSS ?>base.css">
-    <link rel="stylesheet" href="<?= URL_CSS ?>header.css">
-    <link rel="stylesheet" href="<?= URL_CSS ?>reset.css">
-    <link rel="stylesheet" href="<?= URL_CSS ?>home.css">
-    <link rel="stylesheet" href="<?= URL_CSS ?>footer.css">
-</head>
-<body>
-    <!-- Header -->
-    <?php require_once VIEW."/shared/header.php" ?>
-    
-    <!-- Cakes -->
+<!-- Cakes by cate-->
     <div class="wraper">
         <div class="container">
             <div class="sweeties">
@@ -56,8 +35,3 @@
             </div>
         </div>
     </div>
-
-    <!-- Footer -->
-    <?php require_once VIEW."/shared/footer.php" ?>
-</body>
-</html>
