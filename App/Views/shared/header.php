ab3c2f0638cb25b6fed3286548ebb1370d5aa206
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2021-08-12 20:40:57 +0700

    Update: URL of mobile menu

--- App/Views/shared/header.php
+++ App/Views/shared/header.php
@@ -42,12 +42,12 @@
                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
                             </label>
                             <ul class="header__mobile-nav-list">
-                                <li class="header__mobile-nav-link">Home</li>
-                                <li class="header__mobile-nav-link">Cakes</li>
-                                <li class="header__mobile-nav-link">About</li>
-                                <li class="header__mobile-nav-link">Profile</li>
-                                <li class="header__mobile-nav-link">Cart</li>
-                                <li class="header__mobile-nav-link">Log out</li>
+                                <a href="<?= DOCUMENT_ROOT ?>/home"><li class="header__mobile-nav-link">Home</li></a>
+                                <a href="<?= DOCUMENT_ROOT ?>/cakes"><li class="header__mobile-nav-link">Cakes</li></a>
+                                <a href=""><li class="header__mobile-nav-link">About</li></a>
+                                <a href="<?= DOCUMENT_ROOT ?>/users"><li class="header__mobile-nav-link">Profile</li></a>
+                                <a href=""><li class="header__mobile-nav-link">Cart</li></a>
+                                <a href="<?= DOCUMENT_ROOT ?>/accounts"><li class="header__mobile-nav-link">Log out</li></a>
                             </ul>
                         </div>
                     </div>
