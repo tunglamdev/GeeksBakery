9e402278fb4b05b0a4cf8c88fa5b0a5255c0c202
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2021-08-24 13:15:42 +0700

    Update: Admin sidebar: (change icons)

--- App/Configs/Admin_SideBar.php
+++ App/Configs/Admin_SideBar.php
@@ -10,21 +10,21 @@
             "cake" => [
                 "title" => "Manage Cakes",
                 "name" => "cake",
-                "icon" => "nav-icon fas fa-tachometer-alt",
+                "icon" => "nav-icon fas fa-birthday-cake",
                 "link" => DOCUMENT_ROOT."/admin/cakes"
             ],
 
             "categories" => [
                 "title" => "Manage Categories",
                 "name" => "categories",
-                "icon" => "nav-icon fas fa-tachometer-alt",
+                "icon" => "nav-icon fas fa-hamburger",
                 "link" => DOCUMENT_ROOT."/admin/categories"
             ],
 
             "orders" => [
                 "title" => "Manage Orders",
                 "name" => "order",
-                "icon" => "nav-icon fas fa-tachometer-alt",
+                "icon" => "nav-icon fas fa-clipboard-list",
                 "link" => DOCUMENT_ROOT."/admin/orders"
             ]
         ]
